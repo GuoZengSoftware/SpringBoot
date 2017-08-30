40ca55434eadc5d4ad4d1f785cb2f5856fe6d0d0
WangGenshen <847315251@qq.com>
2017-08-30 22:21:07 +0800

    使用IDEA创建的SpringBoot项目，包含了WEB即SpringMVC， Tomcat，MySQL

---
+++ src/main/java/zywork/top/IndexController.java
@@ -0,0 +1,24 @@
+package zywork.top;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Wang Genshen on 2017-08-30.
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(value = "index")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @RequestMapping(value = "index1", produces = "text/plain;charset=utf-8")
+    public String index1() {
+        return "spring boot!";
+    }
+}
---
+++ src/test/java/zywork/top/ZyworkSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package zywork.top;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZyworkSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
